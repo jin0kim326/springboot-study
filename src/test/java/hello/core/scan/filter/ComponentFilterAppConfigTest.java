54e8e5028bed43402e60a522f6de0a4ebc1c962d
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-11-05 11:04:22 +0900

    ComponentScan filter

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,32 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+
+    @Test
+    void componentFilterConfigTest() {
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+
+    }
+
+    @Configuration
+    @ComponentScan(
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class),
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+
+    }
+}
